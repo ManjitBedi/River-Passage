4fb32723e8c90682af9f6abb367b9c40b59db843
Manjit Bedi <dev@manjitbedi.com>
2016-10-16 10:15:54 -0700

    Initial Commit

---
+++ River Passage/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  River Passage
+//
+//  Created by Manjit Bedi on 2016-10-16.
+//  Copyright © 2016 noorg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
