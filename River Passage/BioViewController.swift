6f6dbc1ac03b902be8b80b80a47504537b571ab8
Manjit Bedi <dev@manjitbedi.com>
2016-10-17 20:36:44 -0700

    more fixes

--- River Passage/BioViewController.swift
+++ River Passage/BioViewController.swift
@@ -9,18 +9,17 @@
 import UIKit
 
 class BioViewController: UIViewController {
+    
+    @IBOutlet weak var biographyTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        biographyTextView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
     }
     
+    override func viewDidLayoutSubviews() {
+        biographyTextView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+    }
 
     /*
     // MARK: - Navigation
